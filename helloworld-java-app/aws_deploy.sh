ebc6f74ba9b58724b38ef8647bbaff0199fbecb2
Stein Inge Morisbak <steinim@gmail.com>
2017-02-05 18:28:46 +0100

    added igw and db stuff

--- helloworld-java-app/aws_deploy.sh
+++ helloworld-java-app/aws_deploy.sh
@@ -3,5 +3,7 @@
 vpc=$(envchain aws_bekk aws ec2 describe-vpcs --filters "Name=tag-value, Values=boxfuse_vpc" | jq '.Vpcs[0].VpcId' | sed s/\"//g)
 security_group=$(envchain aws_bekk aws ec2 describe-security-groups --filters "Name=vpc-id, Values=${vpc}" | jq '.SecurityGroups[0].GroupId' | sed s/\"//g)
 subnets=$(envchain aws_bekk aws ec2 describe-subnets --filters "Name=vpc-id, Values=${vpc}, Name=tag-value, Values=boxfuse_vpc_subnet*" | jq '.Subnets[].SubnetId' | tr '\n' ',' | sed s/,$// | sed s/\"//g)
+db_url=$(envchain aws_bekk aws rds describe-db-instances --filters "Name=db-instance-id, Values=hello" | jq '.DBInstances[].Endpoint.Address' | sed s/\"//g)
 
-echo "boxfuse cfg -env=test -vpc=${vpc} -securitygroup=${security_group} -subnets=${subnets}"
+echo "boxfuse create -env=test -vpc=${vpc} -securitygroup=${security_group} -subnets=${subnets} -db.type=none -db.default.url=${db_url} -db.default.username=hello"
+echo "boxfuse run -env=test"
