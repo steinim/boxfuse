659e1f6ddba2f5e2cf4764a6528c87b30acd1c0f
Stein Inge Morisbak <steinim@gmail.com>
2017-02-06 08:58:38 +0100

    destroy route table and wait for rds deletion

---
+++ ansible/bin/wait_for_rds_deletion.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+vpc=$(envchain aws_bekk aws ec2 describe-vpcs --filters "Name=tag-value, Values=boxfuse_vpc" | jq '.Vpcs[0].VpcId' | sed s/\"//g)
+db_status="unknown"
+
+db_status=$(envchain aws_bekk aws rds describe-db-instances --filters "Name=db-instance-id, Values=hello" | jq '.DBInstances[].DBInstanceStatus' | sed s/\"//g)
+
+while [  "${db_status}" == "deleting" ]; do
+  sleep 1
+  db_status=$(envchain aws_bekk aws rds describe-db-instances --filters "Name=db-instance-id, Values=hello" | jq '.DBInstances[].DBInstanceStatus' | sed s/\"//g)
+  echo db status is ${db_status}
+done
